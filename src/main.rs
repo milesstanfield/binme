52d9d7d5afca15ebbd44b1bb64e1e047ec4076e8
Miles Stanfield <milesstanfield@gmail.com>
2023-11-15 23:44:18 -0500

    initial binme work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
+use std::env;
+
+const VALID_CLIS: [&str; 1] = ["doc"];
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().skip(1).collect();
+    match args.len() {
+        0 => handle_no_args(),
+        _ => handle_first_arg(args[0].as_str()),
+    }
+}
+
+fn handle_first_arg(first_arg: &str) {
+    match VALID_CLIS.contains(&first_arg) {
+        true => handle_valid_cli(&first_arg),
+        false => handle_unknown_cli(&first_arg),
+    }
+}
+
+fn handle_valid_cli(cli: &str) {
+    println!("todo doc was first arg {:?}", cli)
+}
+
+fn handle_unknown_cli(cli: &str) {
+    println!("todo unknown cli {:?}", cli)
+}
+
+fn handle_no_args() {
+    println!("called binme without any args")
 }
